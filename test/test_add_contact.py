845eff8f5a2d90010f094567cf73ffbc262d1cf4
Ksusha Barkovskaya <ksusha2626@gmail.com>
2019-06-08 22:46:40 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,11 +12,11 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact()
     app.contact_form(
             Contact(name="Kseniya", middlename="ksu", lastname="Barkovskaya", nickname="ksu26", title="wow",
                     company="fabr", address="Moscow", home_tel="123",
                     mobile_tel="345", work_tel="678", fax="909", email="ksu2@mail.org", page="www.ksu.ru", bday="6",
                     bmonth="March", byear="1995"))
-    app.logout()
+    app.session.logout()
