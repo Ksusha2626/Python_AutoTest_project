283d54c3124327267fb4b2f5bf571f48b5264573
Ksusha Barkovskaya <ksusha2626@gmail.com>
2019-07-04 23:18:22 +0300

    Опции командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,19 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-#        self.wd.implicitly_wait(1)
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized type of browser - %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +30,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("add")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def stop(self):
         self.wd.quit()
